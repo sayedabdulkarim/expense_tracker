4fa6462b86a98936bc96dcbf430433cebc3ccdaf
Sayed Abdul <sakarim9124@gmail.com>
2020-04-03 08:50:59 +0530

    app_setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+
+import App from './components/App.js'
+
+ReactDOM.render(<App />, document.getElementById('app'))
